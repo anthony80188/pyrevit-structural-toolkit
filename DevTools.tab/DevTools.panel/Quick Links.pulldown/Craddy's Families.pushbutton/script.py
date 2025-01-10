15de3a3102f36ebb588e633d5553ac1e8422000e
anthony80188 <Anthonybell80188@outlook.com>
2025-01-10 16:44:57 +0000

    Delete DevTools.tab/DevTools.panel directory

--- DevTools.tab/DevTools.panel/Quick Links.pulldown/Craddy's Families.pushbutton/script.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.Popen(r'explorer /select,"S:\Revit\3 Family Template Files\Craddys\Log"')
--- DevTools.tab/DevTools.panel/Quick Links.pulldown/Revit 2022 Families.pushbutton/script.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.Popen(r'explorer /select,"C:\ProgramData\Autodesk\RVT 2022\Libraries\English\UK\Annotations"')
