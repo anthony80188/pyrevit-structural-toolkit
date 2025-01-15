ac4393aada16957b89b6f63ab2a4c4891414bf5a
anthony80188 <Anthonybell80188@outlook.com>
2025-01-15 15:54:03 +0000

    Add files via upload

---
+++ DevTools.tab/Drawing Tools.panel/3D Modeling.pulldown/JoinWithIntersecting.pushbutton/JoinWithIntersecting_script.py
@@ -0,0 +1,36 @@
+import revitron
+from revitron import _
+from pyrevit import script
+
+output = script.get_output()
+
+__title__ = "Batch Join By Selection"
+
+transaction = revitron.Transaction()
+
+for element in revitron.Selection.get():
+	_element = _(element)
+	if _element.isNotOwned():
+		for intersected in revitron.Filter().byIntersection(element).getElements():
+			if not revitron.DB.JoinGeometryUtils.AreElementsJoined(
+			    revitron.DOC,
+			    element,
+			    intersected
+			):
+				try:
+					if _(intersected).isNotOwned:
+						revitron.DB.JoinGeometryUtils.JoinGeometry(
+						    revitron.DOC,
+						    element,
+						    intersected
+						)
+					else:
+						print(
+						    'Element {} is used by another user!'.format(
+						        output.linkify(intersected.Id)
+						    )
+						)
+				except:
+					pass
+
+transaction.commit()
