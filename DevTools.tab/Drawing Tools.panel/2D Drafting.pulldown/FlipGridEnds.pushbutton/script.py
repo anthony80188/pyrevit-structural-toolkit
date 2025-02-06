5a7b0e708e0069b8b245153b6b594af9f17ddf30
anthony80188 <Anthonybell80188@outlook.com>
2025-02-06 15:27:06 +0000

    Add files via upload

--- DevTools.tab/Drawing Tools.panel/2D Drafting.pulldown/FlipGridEnds.pushbutton/script.py
+++ DevTools.tab/Drawing Tools.panel/2D Drafting.pulldown/FlipGridEnds.pushbutton/script.py
@@ -12,21 +12,13 @@ except:
     from pyrevit import versionmgr
     PYREVIT_VERSION = versionmgr.get_pyrevit_version()
 
-pyRevitNewer44 = PYREVIT_VERSION.major >=4 and PYREVIT_VERSION.minor >=5
-
-if pyRevitNewer44:
-    from pyrevit import script, revit
-    output = script.get_output()
-    logger = script.get_logger()
-    linkify = output.linkify
-    doc = revit.doc
-    uidoc = revit.uidoc
-    selection = revit.get_selection()
-else:
-    from scriptutils import logger, this_script
-    from revitutils import doc, selection, uidoc
-
-    output = this_script.output
+from pyrevit import script, revit
+output = script.get_output()
+logger = script.get_logger()
+linkify = output.linkify
+doc = revit.doc
+uidoc = revit.uidoc
+selection = revit.get_selection()
 
 
 from Autodesk.Revit.DB import Grid, DatumEnds, Transaction
