d1463e3cb60511a080e362fbcda24d64f5de9a5d
anthony80188 <Anthonybell80188@outlook.com>
2025-02-14 17:03:15 +0000

    Update script.py

--- DevTools.tab/Model Management.panel/Print Sheets.pushbutton/script.py
+++ DevTools.tab/Model Management.panel/Print Sheets.pushbutton/script.py
@@ -351,14 +351,14 @@ class EditNamingFormatsWindow(forms.WPFWindow):
     @staticmethod
     def get_default_naming_formats():
         return [
-            NamingFormat(
-                name='Craddys (Revit 2023-)',
-                template='{proj_number}-{sheet_param:Originator}-{sheet_param:Volume or System}-{sheet_param:Levels and Location}-{sheet_param:Type}-{sheet_param:Role}-{sheet_param:Sheet Number} {rev_number} {sheet_param:Sheet Name}.pdf',
+             NamingFormat(
+                name='New Template: BS EN ISO 19650-2-2018 (+A1 2021),
+                template='{proj_number}-{sheet_param:Originator}-{sheet_param:Functional Breakdown}-{sheet_param:Spatial Breakdown}-{sheet_param:Form}-{sheet_param:Discipline}-{sheet_param:Sheet Number}-{rev_number} {sheet_param:Sheet Name}{sheet_param:Drawing Title 2}{sheet_param:Drawing Title 3}.pdf',
                 builtin=True
             ),
             NamingFormat(
-                name='Craddys (Revit 2023+)',
-                template='{proj_number}-{sheet_param:Originator}-{sheet_param:Functional Breakdown}-{sheet_param:Spatial Breakdown}-{sheet_param:Form}-{sheet_param:Discipline}-{sheet_param:Sheet Number} {rev_number} {sheet_param:Sheet Name}{sheet_param:Drawing Title 2}{sheet_param:Drawing Title 3}.pdf',
+                name='Old Template: BS EN ISO 19650-2-2018',
+                template='{proj_number}-{sheet_param:Originator}-{sheet_param:Volume or System}-{sheet_param:Levels and Location}-{sheet_param:Type}-{sheet_param:Role}-{sheet_param:Sheet Number} {rev_number} {sheet_param:Sheet Name}.pdf',
                 builtin=True
             ),
             NamingFormat(
