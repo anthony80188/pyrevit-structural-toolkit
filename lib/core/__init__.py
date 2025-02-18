2f601c4d77e98785d8eff8d6074fcb277662f6fd
anthony80188 <Anthonybell80188@outlook.com>
2025-02-18 11:34:21 +0000

    Add files via upload

--- lib/core/__init__.py
+++ lib/core/__init__.py
@@ -1,18 +1,10 @@
 # extension library file
 
-import os
-import sys
 import os.path as op
 import urllib2
-import selectionhelpers
+from System import Version
 
-from pyrevit import EXEC_PARAMS
-from pyrevit import script
-from pyrevit import forms
-from pyrevit.loader import sessionmgr
-from pyrevit.loader import sessioninfo
-
-class Core :
+class Core(object):
    """
    Extension Coreclass
    """
@@ -21,32 +13,23 @@ class Core :
       A_EXT_DIR = op.dirname(op.dirname(__file__))
       return A_EXT_DIR
 
-
-
-
 def get_local_version():
     with open(op.join(op.dirname(__file__), 'version'), 'r') as version_file:
        version = version_file.read()
     return version
 
-def start_reload():
-   logger = script.get_logger()
-   results = script.get_results()
-   
-   # re-load pyrevit session.
-   logger.info('Reloading....')
-   sessionmgr.reload_pyrevit()
-   results.newsession = sessioninfo.get_session_uuid()
-
 # Get version inside the github repository
 def get_git_version():
-   data = urllib2.urlopen('https://raw.githubusercontent.com/romangolev/pyArchitect/main/lib/core/version')
+   data = urllib2.urlopen('https://raw.githubusercontent.com/wemyssj/Dev-tools/main/lib/core/version')
    for line in data: 
       gitversion = line
    return gitversion
 
 def update_needed():
-   if get_local_version() != get_git_version():
-      return True
-   else:
+   try:
+      if Version(get_local_version()) < Version(get_git_version()):
+         return True
+      else:
+         return False
+   except Exception:
       return False
