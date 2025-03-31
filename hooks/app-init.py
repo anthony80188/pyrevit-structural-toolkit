95dfb903212d11ecef423ad2ab5d4bb8f5d53eef
anthony80188 <Anthonybell80188@outlook.com>
2025-03-31 09:05:31 +0100

    Update app-init.py

--- hooks/app-init.py
+++ hooks/app-init.py
@@ -9,8 +9,8 @@ if msgUtils_muted():
 
 # Get icon file (doesn't work)
 # curPath = script.get_script_path()
-# remPath = curPath.split('guRoo.tab')[0]
+# remPath = curPath.split('DevTools.tab')[0]
 # icoFile = remPath + r'bin\Graphics\ico256_guRoo.ico'
 
 # Display the message to the user
-forms.toast("Toolbar has been loaded!","guRoo for Revit",appid="guRoo",actions={"BIM Guru course platform":"https://courses.bimguru.education/"})
+forms.toast("Toolbar has been loaded!","CDY DevTools for Revit",appid="DevTools",actions={"Respository":"https://github.com/WemyssJ/"})
