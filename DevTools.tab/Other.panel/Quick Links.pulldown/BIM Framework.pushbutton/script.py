085f490fe5bdbf3f22e78abc0f69f25e57f378c4
anthony80188 <Anthonybell80188@outlook.com>
2025-04-02 12:06:42 +0100

    Delete DevTools.tab/Other.panel/Quick Links.pulldown directory

--- DevTools.tab/Other.panel/Quick Links.pulldown/BIM Framework.pushbutton/script.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.Popen(r'explorer /select,"Z:\00001000\0165\Library\British Standards\BS-UK BIM Framework Solution Pack\Dynamo Path"')
--- DevTools.tab/Other.panel/Quick Links.pulldown/Craddy's User Guides.pushbutton/script.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.Popen(r'explorer /select,"\\Cpd.local\z\00001000\0100\07 Craddys User Guides & BOP support documentation\Dynamo Path"')
