f3ef847b6be16eab070dfe6665b7209ca452ad5e
anthony80188 <Anthonybell80188@outlook.com>
2025-05-20 21:14:43 +0100

    Add files via upload

---
+++ DevTools.tab/Rebar.panel/Guides.pulldown/SMo Detailing.pushbutton/script.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open('https://drive.google.com/file/d/1DCIYB1rnr6OXNCLG42kvm_2_EYYKikOr/view?usp=sharing')
---
+++ DevTools.tab/Rebar.panel/Selection.pulldown/Match SM to RN.pushbutton/match_SM_to _RN._script.py
@@ -0,0 +1,24 @@
+from Autodesk.Revit.DB import Transaction, BuiltInParameter
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+
+# Your RebarSelector is assumed to work correctly:
+from rebar_selector import RebarSelector
+rs = RebarSelector(doc, uidoc)
+rebar_collector = rs.get_rebars()
+
+t = Transaction(doc, "Set Schedule Mark to Rebar Number")
+t.Start()
+
+for rebar in rebar_collector:
+    # Get Rebar Number (string)
+    rebarNumberParam = rebar.get_Parameter(BuiltInParameter.REBAR_NUMBER)
+    scheduleMarkParam = rebar.get_Parameter(BuiltInParameter.REBAR_ELEM_SCHEDULE_MARK)
+    
+    if rebarNumberParam and scheduleMarkParam:
+        rebarNumber = rebarNumberParam.AsString()
+        if rebarNumber:  # Ensure it's not None or empty
+            scheduleMarkParam.Set(rebarNumber)
+
+t.Commit()
