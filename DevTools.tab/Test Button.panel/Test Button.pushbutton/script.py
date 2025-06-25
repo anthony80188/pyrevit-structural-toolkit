7effca657c844609e4fc2b4de9fef29b6558103e
anthony80188 <Anthonybell80188@outlook.com>
2025-06-25 11:42:41 +0100

    Add files via upload

--- DevTools.tab/Test Button.panel/Test Button.pushbutton/script.py
+++ DevTools.tab/Test Button.panel/Test Button.pushbutton/script.py
@@ -10,12 +10,15 @@ from System import Environment
 # import pyrevit libraries
 from pyrevit import forms,revit,DB
 
+if __shiftclick__:  
+    AppDataList = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).split("\\")
+    AppDataList.pop(-1)
+    AppData = "\\".join(AppDataList)
+    button_location = AppData + "\Roaming\pyRevit\Extensions\DevTools.extension\DevTools.tab\Test Button.panel\Test Button.pushbutton"
+    print(button_location)
 
-AppDataList = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).split("\\")
-AppDataList.pop(-1)
-AppData = "\\".join(AppDataList)
-button_location = AppData + "\Roaming\pyRevit\Extensions\DevTools.extension\DevTools.tab\Test Button.panel\Test Button.pushbutton"
-print(button_location)
+    os.startfile(button_location)
+else:
+    print("No script added to test button. Shift click to navigate to file location")
 
-os.startfile(button_location)
 
