0cbc686a80a25905ce2a3b2a69192518f9a2e4a7
anthony80188 <Anthonybell80188@outlook.com>
2025-07-03 15:13:49 +0100

    Add files via upload
    Find Rev from drop down list

--- DevTools.tab/Model Management.panel/Revision.pulldown/Find All Sheets With Selected Revision.pushbutton/script.py
+++ DevTools.tab/Model Management.panel/Revision.pulldown/Find All Sheets With Selected Revision.pushbutton/script.py
@@ -1,18 +1,13 @@
-from pyrevit import revit, DB
+from pyrevit import revit, DB, forms
 
-
-selection = revit.get_selection()
-
-selectedrevs = []
+selectedrevseq = []
 hasSelectedRevision = False
-multipleRevs = False
 
-for s in selection:
-    if isinstance(s, DB.RevisionCloud):
-        selectedrevs.append(s.RevisionId.IntegerValue)
+revisions = forms.select_revisions(button_name='Select Revision',
+                                   multiple=True)
 
-if len(selectedrevs) > 1:
-    multipleRevs = True
+for r in revisions:
+    selectedrevseq.append(r.SequenceNumber)
 
 print('REVISED SHEETS:\n\nNAME\tNUMBER\n' + '-'*70)
 
@@ -26,15 +21,13 @@ sheets = sorted(sheetsnotsorted, key=lambda x: x.SheetNumber)
 for s in sheets:
     hasSelectedRevision = False
     revision_ids = s.GetAllRevisionIds()
-    revids = [x.IntegerValue for x in revision_ids]
-    for sr in selectedrevs:
-        if sr in revids:
+    for rev_id in revision_ids:
+        rev = revit.doc.GetElement(rev_id)
+        if rev.SequenceNumber in selectedrevseq:
             hasSelectedRevision = True
+            break
+
     if hasSelectedRevision:
         print('{0}\t{1}'.format(s.Parameter[DB.BuiltInParameter.SHEET_NUMBER].AsString(),
                                 s.Parameter[DB.BuiltInParameter.SHEET_NAME].AsString()))
 
-        if multipleRevs:
-            for revid in revision_ids:
-                rev = revit.doc.GetElement(revid)
-                revit.report.print_revision(rev, prefix='\t\t', print_id=False)
