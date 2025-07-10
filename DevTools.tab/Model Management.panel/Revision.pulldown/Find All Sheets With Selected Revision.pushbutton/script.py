c40c47f3a68fcbd087c56a06f59c7ea5ea5e46f5
anthony80188 <Anthonybell80188@outlook.com>
2025-07-10 16:43:35 +0100

    Update script.py

--- DevTools.tab/Model Management.panel/Revision.pulldown/Find All Sheets With Selected Revision.pushbutton/script.py
+++ DevTools.tab/Model Management.panel/Revision.pulldown/Find All Sheets With Selected Revision.pushbutton/script.py
@@ -1,26 +1,38 @@
 from pyrevit import revit, DB, forms
 
 selectedrevseq = []
-hasSelectedRevision = False
+selectedrevinfo = []
 
-revisions = forms.select_revisions(button_name='Select Revision',
-                                   multiple=True)
+# Prompt user to select revisions
+revisions = forms.select_revisions(button_name='Select Revision', multiple=True)
 
+# Gather sequence numbers and descriptions
 for r in revisions:
     selectedrevseq.append(r.SequenceNumber)
+    rev_date = r.RevisionDate if r.RevisionDate else "N/A"
+    description = r.Description if r.Description else "No Description"
+    selectedrevinfo.append('Sequence {0}: {1} (Date: {2})'.format(r.SequenceNumber, description, rev_date))
 
-print('REVISED SHEETS:\n\nNAME\tNUMBER\n' + '-'*70)
+# Print selected revision info
+print('SEARCHING FOR SHEETS THAT CONTAIN ATLEAST ONE OF SELECTED REVISION IDS:')
+for info in selectedrevinfo:
+    print(info)
 
+print('\nNAME\tNUMBER\n' + '-' * 70)
+
+# Collect and sort sheets
 sheetsnotsorted = DB.FilteredElementCollector(revit.doc)\
-                    .OfCategory(DB.BuiltInCategory.OST_Sheets)\
-                    .WhereElementIsNotElementType()\
-                    .ToElements()
+    .OfCategory(DB.BuiltInCategory.OST_Sheets)\
+    .WhereElementIsNotElementType()\
+    .ToElements()
 
 sheets = sorted(sheetsnotsorted, key=lambda x: x.SheetNumber)
 
+# Print sheets that include selected revisions
 for s in sheets:
     hasSelectedRevision = False
     revision_ids = s.GetAllRevisionIds()
+
     for rev_id in revision_ids:
         rev = revit.doc.GetElement(rev_id)
         if rev.SequenceNumber in selectedrevseq:
@@ -28,6 +40,6 @@ for s in sheets:
             break
 
     if hasSelectedRevision:
-        print('{0}\t{1}'.format(s.Parameter[DB.BuiltInParameter.SHEET_NUMBER].AsString(),
-                                s.Parameter[DB.BuiltInParameter.SHEET_NAME].AsString()))
-
+        print('{0}\t{1}'.format(
+            s.Parameter[DB.BuiltInParameter.SHEET_NUMBER].AsString(),
+            s.Parameter[DB.BuiltInParameter.SHEET_NAME].AsString()))
