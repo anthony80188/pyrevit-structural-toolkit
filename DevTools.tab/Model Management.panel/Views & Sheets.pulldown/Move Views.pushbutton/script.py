31aca274fab52d014e396f5a8ad0628965b4297a
anthony80188 <Anthonybell80188@outlook.com>
2025-07-23 09:58:44 +0100

    Update script.py

--- DevTools.tab/Model Management.panel/Views & Sheets.pulldown/Move Views.pushbutton/script.py
+++ DevTools.tab/Model Management.panel/Views & Sheets.pulldown/Move Views.pushbutton/script.py
@@ -3,7 +3,6 @@ from pyrevit import revit, DB, UI
 from pyrevit import forms
 from pyrevit import script
 
-
 cursheet = revit.active_view
 forms.check_viewtype(cursheet, DB.ViewType.DrawingSheet, exitscript=True)
 
@@ -19,27 +18,65 @@ if dest_sheet:
     for el in sel:
         selected_vports.append(el)
 
+    results = []
+
     if len(selected_vports) > 0:
         with revit.Transaction('Move Viewports'):
+            # Collect existing detail numbers on destination sheet
+            existing_detail_numbers = set()
+            dest_vports = DB.FilteredElementCollector(revit.doc, dest_sheet.Id)\
+                            .OfClass(DB.Viewport)\
+                            .ToElements()
+            for v in dest_vports:
+                num = v.get_Parameter(DB.BuiltInParameter.VIEWPORT_DETAIL_NUMBER).AsString()
+                if num:
+                    existing_detail_numbers.add(num)
+
             for vp in selected_vports:
                 if isinstance(vp, DB.Viewport):
                     viewId = vp.ViewId
                     vpCenter = vp.GetBoxCenter()
                     vpTypeId = vp.GetTypeId()
+                    view = revit.doc.GetElement(viewId)
+                    viewname = view.Name
+
+                    original_detail_number = vp.get_Parameter(DB.BuiltInParameter.VIEWPORT_DETAIL_NUMBER).AsString()
+
+                    # Delete the original viewport
                     cursheet.DeleteViewport(vp)
-                    nvp = DB.Viewport.Create(revit.doc,
-                                             dest_sheet.Id,
-                                             viewId,
-                                             vpCenter)
+
+                    # Create new viewport
+                    nvp = DB.Viewport.Create(revit.doc, dest_sheet.Id, viewId, vpCenter)
                     nvp.ChangeTypeId(vpTypeId)
+
+                    # Try to assign the same detail number
+                    param = nvp.get_Parameter(DB.BuiltInParameter.VIEWPORT_DETAIL_NUMBER)
+                    assigned_number = ""
+                    if param and not param.IsReadOnly:
+                        if original_detail_number not in existing_detail_numbers:
+                            param.Set(original_detail_number)
+                            assigned_number = original_detail_number
+                            existing_detail_numbers.add(original_detail_number)
+                            results.append("Detail: " + original_detail_number + " / View Name:" + viewname + " moved from sheet " + cursheet.SheetNumber + " to " + dest_sheet.SheetNumber + " and detail number preserved.")
+                        else:
+                            # Let Revit auto-assign a new detail number
+                            assigned_number = param.AsString()
+                            results.append("Detail: " + original_detail_number + " / View Name:" + viewname + " moved from sheet " + cursheet.SheetNumber + " to " + dest_sheet.SheetNumber + " with new detail number " + assigned_number + " (original '" + original_detail_number + "' already in use).")
+                    else:
+                        results.append("Detail: '" + viewname + "' moved but detail number could not be set.")
+
                 elif isinstance(vp, DB.ScheduleSheetInstance):
-                    nvp = \
-                        DB.ScheduleSheetInstance.Create(
-                            revit.doc, dest_sheet.Id, vp.ScheduleId, vp.Point
-                            )
+                    nvp = DB.ScheduleSheetInstance.Create(revit.doc, dest_sheet.Id, vp.ScheduleId, vp.Point)
                     revit.doc.Delete(vp.Id)
+                    results.append("Schedule '" + vp.Name + "' moved from sheet " + cursheet.SheetNumber + " to " + dest_sheet.SheetNumber + ".")
+
+        # Print output summary
+        output = script.get_output()
+        output.print_md("### Move Viewport Results:")
+        for line in results:
+            output.print_md("- " + line)
+
     else:
         forms.alert('At least one viewport must be selected.')
 else:
-    forms.alert('You must select at least one sheet to add '
-                'the selected viewports to.')
+    forms.alert('You must select at least one sheet to add the selected viewports to.')
