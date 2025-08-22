234694b560f37d179f6a54f4c5351cbce193141b
anthony80188 <Anthonybell80188@outlook.com>
2025-08-22 16:05:02 +0100

    Update script.py

--- DevTools.tab/Model Management.panel/Printer.pushbutton/script.py
+++ DevTools.tab/Model Management.panel/Printer.pushbutton/script.py
@@ -386,7 +386,7 @@ class EditNamingFormatsWindow(forms.WPFWindow):
             ),
             NamingFormat(
                 name='Aldi: BS1192:2007+A2:2016',
-                template='{proj_number}-{sheet_param:PM.Sheet.Title.Creator.Originator}-{sheet_param:PM.Sheet.Title.View.Zone}-{sheet_param:PM.Sheet.Title.View.Level}-{sheet_param:PM.Sheet.Title.View.Type}-{sheet_param:PM.Sheet.Title.Creator.Role}-{sheet_param:Sheet Number}-{rev_number}-{sheet_param:Sheet Name}{sheet_param:Drawing Title 2}{sheet_param:Drawing Title 3}.pdf',
+                template='{proj_param:PM.Sheet.Title.Number.Project}-{sheet_param:PM.Sheet.Title.Creator.Originator}-{sheet_param:PM.Sheet.Title.View.Zone}-{sheet_param:PM.Sheet.Title.View.Level}-{sheet_param:PM.Sheet.Title.View.Type}-{sheet_param:PM.Sheet.Title.Creator.Role}-{sheet_param:Sheet Number}-{rev_number}-{sheet_param:Sheet Name}{sheet_param:Drawing Title 2}{sheet_param:Drawing Title 3}.pdf',
                 builtin=True
             ),
             NamingFormat(
@@ -1649,4 +1649,5 @@ if __shiftclick__:  #pylint: disable=E0602
             cleanup_sheetnumbers(open_doc)
 else:
     PrintSheetsWindow('PrintSheets.xaml').ShowDialog()
+
 
