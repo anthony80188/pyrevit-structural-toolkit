a198dd3910da0452ca805a395205e109fc992dc1
anthony80188 <Anthonybell80188@outlook.com>
2025-08-29 14:02:00 +0100

    Update app-init.py

--- hooks/app-init.py
+++ hooks/app-init.py
@@ -9,8 +9,8 @@ if msgUtils_muted():
 
 # Get icon file (doesn't work)
 # curPath = script.get_script_path()
-# remPath = curPath.split('DevTools.tab')[0]
-# icoFile = remPath + r'bin\Graphics\ico256_guRoo.ico'
+# remPath = curPath.split('CDY-ProTools.tab')[0]
+# icoFile = remPath + r'bin\Graphics\ico256_CDY.ico'
 
 # Display the message to the user
-forms.toast("Toolbar has been loaded!","CDY DevTools for Revit",appid="DevTools",actions={"Respository":"https://github.com/WemyssJ/"})
+forms.toast("Toolbar has been loaded!","CDY-ProTools for Revit",appid="BIMTools",actions={"Respository":"https://github.com/WemyssJ/"})
