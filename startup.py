473767bc4b1d74492f7023534bdf461e33548687
anthony80188 <Anthonybell80188@outlook.com>
2025-10-09 14:38:39 +0100

    Add files via upload

--- startup.py
+++ startup.py
@@ -1,157 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Checks GitHub for update.
-Clones a panel to the Modify tab when a document is opened.
-Shows a popup listing CDY-ProTools panels on startup.
-Hides the "Other" panel for unauthorized users.
+CDY-ProTools Startup: Developer Panel lock state
+- Enables/disables Developer panel and pyRevit tab on Revit launch
 """
 
-# --- Imports ---
 import clr
 clr.AddReference('AdWindows')
 import Autodesk.Windows as AdWindows
-clr.AddReference('System.Windows.Forms')
-from System.Windows.Forms import SendKeys
+import os, json
 
-from pyrevit import HOST_APP, forms
-from pyrevit.coreutils import logger
-from System import EventHandler
-from Autodesk.Revit.UI.Events import IdlingEventArgs
-from Autodesk.Revit.DB import Events
-
-import os
-import core
-
-# --- Flags ---
-DEBUG_UI = False               # Toggle detailed UI debug output
-
-# --- User Info ---
-windows_username = os.getenv('USERNAME')
-revit_username = HOST_APP.username
-
-# --- Logger ---
-script_logger = logger.get_logger('switchback_api')
-
-# --- Check for updates ---
-try:
-    if core.update_needed():
-        forms.toaster.send_toast("New update for CDY-ProTools extension available: {}".format(core.get_git_version()))
-except Exception:
-    pass
+TAB_NAME = "CDY-ProTools"
+DEV_PANEL_NAME = "Developer"
+PYRVT_TAB_NAME = "pyRevit"
+UNLOCK_FILE = os.path.join(os.getenv("APPDATA"), "CDY-ProTools", "dev_unlock.json")
 
 
-# def find_and_clone_target_panel1():
-#     ribbon = AdWindows.ComponentManager.Ribbon
-#     if not ribbon:
-#         return
-
-#     target_tab = next((tab for tab in ribbon.Tabs 
-#                       if tab.Title == "Modify" and tab.IsVisible), None)
-#     if not target_tab:
-#         return
-
-#     source_panel = None
-#     for tab in ribbon.Tabs:
-#         if not tab.IsVisible:
-#             continue
-#         for panel in tab.Panels:
-#             if panel.Source and panel.Source.Title == "Test Button":
-#                 source_panel = panel
-#                 break
-#         if source_panel:
-#             break
-
-#     if not source_panel:
-#         return
-
-#     if any(panel.Source and panel.Source.Title == source_panel.Source.Title 
-#            for panel in target_tab.Panels):
-#         return
-
-#     target_tab.Panels.Add(AdWindows.RibbonPanel())
-#     new_panel = target_tab.Panels[target_tab.Panels.Count - 1]
-#     new_panel.Source = source_panel.Source.Clone()
-#     new_panel.IsEnabled = True
-
-
-def Hidden_Panel():
-    authorized_users = ["joewemyss", "JamesScrivens", "davebarronVXKZF", "jackmitchell-pardoe", "andrewowen"]
-
-    ribbon = AdWindows.ComponentManager.Ribbon
-    HiddenPanel = "Developer"
-    pyRevitTabName = "pyRevit"  # Change this if your tab has a different title
-
-    if not ribbon:
-        return
-
-    # Hide "Developer" panel in "CDY-ProTools" tab if unauthorized
-    for tab in ribbon.Tabs:
-        if tab.Title == "CDY-ProTools" and tab.IsVisible:
-            for panel in tab.Panels:
-                if panel.Source and panel.Source.Title == HiddenPanel:
-                    if revit_username not in authorized_users:
-                        if panel.IsVisible or panel.IsEnabled:
-                            panel.IsVisible = False
-                            panel.IsEnabled = False
-                            script_logger.info("Unloading {} panel for {}".format(HiddenPanel, revit_username))
-                    else:
-                        if not panel.IsVisible:
-                            script_logger.info("Loading {} panel for {}".format(HiddenPanel, revit_username))
-            # No return here because we want to continue to check other tabs
-
-    # Hide entire pyRevit.extension tab if unauthorized
-    for tab in ribbon.Tabs:
-        if tab.Title == pyRevitTabName:
-            if revit_username not in authorized_users:
-                if tab.IsVisible:
-                    tab.IsVisible = False
-                    script_logger.info("Hiding pyRevit.extension tab for unauthorized user {}".format(revit_username))
-            else:
-                if not tab.IsVisible:
-                    tab.IsVisible = True
-                    script_logger.info("Showing pyRevit.extension tab for authorized user {}".format(revit_username))
-            return
-
-
-def debug_list_panels_ui():
-    ribbon = AdWindows.ComponentManager.Ribbon
-    if not ribbon:
-        script_logger.info("Ribbon not found")
-        return
-
-    for tab in ribbon.Tabs:
-        if tab.Title == "CDY-ProTools":
-            msg = "CDY-ProTools tab found, Visible={}\n\nPanels:\n".format(tab.IsVisible)
-            for panel in tab.Panels:
-                title = panel.Source.Title if panel.Source else "<No Source>"
-                msg += " - '{}' Visible={}\n".format(title, panel.IsVisible)
-            script_logger.info(msg)
-            return
-    script_logger.info("CDY-ProTools tab not found")
-
-
-def on_idling(sender, args):
+def is_unlocked():
+    """Return True if developer panel is unlocked (file may not exist)."""
+    if not os.path.exists(UNLOCK_FILE):
+        # Ensure file exists for first-time startup
+        folder = os.path.dirname(UNLOCK_FILE)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        with open(UNLOCK_FILE, "w") as f:
+            json.dump({"unlocked": False}, f)
+        return False
     try:
-        if DEBUG_UI:
-            debug_list_panels_ui()  # Show popup with panel info
-        Hidden_Panel()              # Hide the Developer panel if needed
-    except Exception as e:
-        script_logger.info("Error during panel debug or hiding:\n{}".format(str(e)))
-    HOST_APP.uiapp.Idling -= on_idling  # Unsubscribe after running once
+        with open(UNLOCK_FILE, "r") as f:
+            data = json.load(f)
+        return data.get("unlocked", False)
+    except Exception:
+        return False
 
 
-def safe_hook_idling():
-    HOST_APP.uiapp.Idling += EventHandler[IdlingEventArgs](on_idling)
+def update_dev_panel_state():
+    """Apply lock state to UI panels."""
+    ribbon = AdWindows.ComponentManager.Ribbon
+    if not ribbon:
+        return
+
+    dev_panel = None
+    pyrvt_tab = None
+
+    for tab in ribbon.Tabs:
+        if tab.Title == TAB_NAME:
+            for panel in tab.Panels:
+                if panel.Source and panel.Source.Title == DEV_PANEL_NAME:
+                    dev_panel = panel
+                    break
+        elif tab.Title == PYRVT_TAB_NAME:
+            pyrvt_tab = tab
+
+    unlocked = is_unlocked()
+
+    # Apply Developer panel state
+    if dev_panel:
+        dev_panel.IsEnabled = unlocked
+        dev_panel.IsVisible = True  # panel is always visible, just disabled if locked
+
+    # Hide pyRevit tab when locked
+    if pyrvt_tab:
+        pyrvt_tab.IsVisible = unlocked  # visible only when unlocked
 
 
-# --- Initial clone on startup ---
-# find_and_clone_target_panel1()
-
-# --- Safe hook to idling event ---
-safe_hook_idling()
-
-# --- Also run clone on document opening ---
-#def doc_opening_handler(sender, args):
-    # find_and_clone_target_panel1()
-
-#HOST_APP.app.DocumentOpening += EventHandler[Events.DocumentOpeningEventArgs](doc_opening_handler)
+# Execute on load
+update_dev_panel_state()
