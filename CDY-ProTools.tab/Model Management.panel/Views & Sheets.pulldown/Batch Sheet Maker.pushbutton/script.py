0bf7b8a9193c596b08c4218ccc0d643e34fd90bf
anthony80188 <Anthonybell80188@outlook.com>
2026-02-16 14:06:10 +0000

    Add files via upload

--- CDY-ProTools.tab/Model Management.panel/Views & Sheets.pulldown/Batch Sheet Maker.pushbutton/script.py
+++ CDY-ProTools.tab/Model Management.panel/Views & Sheets.pulldown/Batch Sheet Maker.pushbutton/script.py
@@ -1,49 +1,64 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
 import re
-
-from pyrevit import coreutils
-from pyrevit import revit, DB
-from pyrevit import forms
-from pyrevit import script
+import clr
+from pyrevit import coreutils, revit, DB, forms, script
 
 ##############################################################################################
-# TELEMETRY IMPORTS #
+# TELEMETRY IMPORTS
 ##############################################################################################
-# Only works IF specified TELEMETRY_JSON path exists within %AppData%\pyRevit\Extensions\BIMTools.extension\lib\telemetry_auto.py"
-# Records tool usage by date & revit version
-import os, sys
-
-# Add lib folder for telemetry_auto
 lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'lib'))
 if lib_path not in sys.path:
     sys.path.append(lib_path)
-
 import telemetry_auto
 
-tool_name = os.path.basename(os.path.dirname(__file__)) 
+tool_name = os.path.basename(os.path.dirname(__file__))
 TOOL_NAME = tool_name.replace(".pushbutton", "")
 telemetry_auto.log_tool_usage(TOOL_NAME)
 ##############################################################################################
 
-
 logger = script.get_logger()
 
 
+# ----------------- DRAWING NUMBER PARSER -----------------
+def parse_drawing_number(drawing_number):
+    """
+    Example drawing number: 13914-CDY-AA-FN-DR-S-101010
+    Returns dictionary mapping to Revit sheet parameters.
+    """
+    parts = drawing_number.split('-')
+    if len(parts) < 7:
+        raise ValueError('Invalid drawing number format: {0}'.format(drawing_number))
+
+    return {
+        "Project Number": parts[0],
+        "Originator": parts[1],
+        "Functional Breakdown": parts[2],
+        "Spatial Breakdown": parts[3],
+        "Form": parts[4],
+        "Discipline": parts[5],
+        "Sheet Number": parts[6]
+    }
+
+
+# ----------------- MAIN WINDOW CLASS -----------------
 class BatchSheetMakerWindow(forms.WPFWindow):
     def __init__(self, xaml_file_name):
         forms.WPFWindow.__init__(self, xaml_file_name)
-        self._sheet_dict = {}
+        self._sheet_dict = {}  # {sheet_num: sheet_name}
         self._titleblock_id = None
+        self._namingProtocol = False
         self.sheets_tb.Focus()
 
+    # ----------------- PROCESS PASTED DATA -----------------
     def _process_sheet_code(self):
         for sheet_code in str(self.sheets_tb.Text).split('\n'):
             if coreutils.is_blank(sheet_code):
                 continue
 
             if '\t' not in sheet_code:
-                logger.warning('Sheet name must be separated from '
-                               'sheet number by a single tab: {}'
-                               .format(sheet_code))
+                logger.Warning('Sheet name must be separated from sheet number by a single tab: {0}'.format(sheet_code))
                 return False
 
             sheet_code = re.sub('\t+', '\t', sheet_code)
@@ -52,70 +67,73 @@ class BatchSheetMakerWindow(forms.WPFWindow):
             try:
                 for range_num in coreutils.extract_range(num):
                     self._sheet_dict[range_num] = name
-            except Exception as range_err:
-                logger.error(range_err)
+            except Exception, range_err:
+                logger.Error(str(range_err))
                 return False
-
         return True
 
+    # ----------------- TITLEBLOCK SELECTION -----------------
     def _ask_for_titleblock(self):
         tblock = forms.select_titleblocks(doc=revit.doc)
         if tblock is not None:
             self._titleblock_id = tblock
             return True
-
         return False
 
-    @staticmethod
-    def _create_placeholder(sheet_num, sheet_name):
-        with DB.Transaction(revit.doc, 'Create Placeholder') as t:
-            try:
-                t.Start()
-                new_phsheet = DB.ViewSheet.CreatePlaceholder(revit.doc)
-                new_phsheet.Name = sheet_name
-                new_phsheet.SheetNumber = sheet_num
-                t.Commit()
-            except Exception as create_err:
-                t.RollBack()
-                logger.error('Error creating placeholder sheet {}:{} | {}'
-                             .format(sheet_num, sheet_name, create_err))
-
+    # ----------------- CREATE SHEET -----------------
     def _create_sheet(self, sheet_num, sheet_name):
-        with DB.Transaction(revit.doc, 'Create Sheet') as t:
-            try:
-                t.Start()
-                new_phsheet = DB.ViewSheet.Create(revit.doc,
-                                                  self._titleblock_id)
-                new_phsheet.Name = sheet_name
-                new_phsheet.SheetNumber = sheet_num
-                t.Commit()
-            except Exception as create_err:
-                t.RollBack()
-                logger.error('Error creating sheet sheet {}:{} | {}'
-                             .format(sheet_num, sheet_name, create_err))
+        t = DB.Transaction(revit.doc, 'Create Sheet')
+        try:
+            t.Start()
+            new_sheet = DB.ViewSheet.Create(revit.doc, self._titleblock_id)
+            new_sheet.Name = sheet_name
+            new_sheet.SheetNumber = sheet_num
 
+            # Only set parameters if real sheet and checkbox is checked
+            if self._namingProtocol:
+                try:
+                    param_values = parse_drawing_number(sheet_num)
+                    for param_name in param_values:
+                        value = param_values[param_name]
+                        param = new_sheet.LookupParameter(param_name)
+                        if param and not param.IsReadOnly:
+                            param.Set(value)
+                except Exception, e:
+                    logger.Warning('Could not set parameters for {0}: {1}'.format(sheet_num, e))
+
+            t.Commit()
+        except Exception, create_err:
+            t.RollBack()
+            logger.Error('Error creating sheet {0}:{1} | {2}'.format(sheet_num, sheet_name, create_err))
+
+    # ----------------- CREATE SHEETS ENTRY POINT -----------------
     def create_sheets(self, sender, args):
         self.Close()
 
-        if self._process_sheet_code():
-            if self.sheet_cb.IsChecked:
-                create_func = self._create_sheet
-                transaction_msg = 'Batch Create Sheets'
-                if not self._ask_for_titleblock():
-                    script.exit()
-            else:
-                create_func = self._create_placeholder
-                transaction_msg = 'Batch Create Placeholders'
+        # Checkbox now controls whether to parse drawing numbers
+        self._namingProtocol = bool(self.NamingProtocol_cb.IsChecked)
 
-            with DB.TransactionGroup(revit.doc, transaction_msg) as tg:
-                tg.Start()
-                for sheet_num, sheet_name in self._sheet_dict.items():
-                    logger.debug('Creating Sheet: {}:{}'.format(sheet_num,
-                                                                sheet_name))
-                    create_func(sheet_num, sheet_name)
-                tg.Assimilate()
-        else:
-            logger.error('Aborted with errors.')
+        # Process pasted data
+        if not self._process_sheet_code():
+            logger.Error("Aborted: invalid TextBox input")
+            script.exit()
+
+        # Ask for titleblock if creating sheets
+        if self.sheet_cb.IsChecked:
+            if not self._ask_for_titleblock():
+                script.exit()
+            create_func = self._create_sheet
+            transaction_msg = 'Batch Create Sheets'
+
+        # Batch creation
+        tg = DB.TransactionGroup(revit.doc, transaction_msg)
+        tg.Start()
+        for sheet_num in self._sheet_dict:
+            sheet_name = self._sheet_dict[sheet_num]
+            logger.debug('Creating Sheet: {0}:{1}'.format(sheet_num, sheet_name))
+            create_func(sheet_num, sheet_name)
+        tg.Assimilate()
 
 
+# ----------------- RUN -----------------
 BatchSheetMakerWindow('BatchSheetMakerWindow.xaml').ShowDialog()
