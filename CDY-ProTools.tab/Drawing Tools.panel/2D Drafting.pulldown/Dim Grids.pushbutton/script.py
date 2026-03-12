f1f51f745c65545252058ff8efebbcde652db743
anthony80188 <Anthonybell80188@outlook.com>
2026-03-12 13:31:07 +0000

    Telementry Updated

--- CDY-ProTools.tab/Drawing Tools.panel/2D Drafting.pulldown/Dim Grids.pushbutton/script.py
+++ CDY-ProTools.tab/Drawing Tools.panel/2D Drafting.pulldown/Dim Grids.pushbutton/script.py
@@ -8,20 +8,24 @@ from Autodesk.Revit.UI.Selection import ISelectionFilter
 from Autodesk.Revit import Exceptions
 import os, sys
 
-# -----------------------------------------------------------------------------------
-# TELEMETRY (unchanged)
-# -----------------------------------------------------------------------------------
+##############################################################################################
+# TELEMETRY IMPORTS #
+##############################################################################################
+# Only works IF specified TELEMETRY_JSON path exists within %AppData%\pyRevit\Extensions\BIMTools.extension\lib\telemetry_auto.py"
+# Records tool usage by date & revit version
+import os, sys
+
+# Add lib folder for telemetry_auto
 lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'lib'))
 if lib_path not in sys.path:
     sys.path.append(lib_path)
 
-try:
-    import telemetry_auto
-    tool_name = os.path.basename(os.path.dirname(__file__))
-    TOOL_NAME = tool_name.replace(".pushbutton", "")
-    telemetry_auto.log_tool_usage(TOOL_NAME)
-except:
-    pass
+import telemetry_auto
+
+tool_name = os.path.basename(os.path.dirname(__file__)) 
+TOOL_NAME = tool_name.replace(".pushbutton", "")
+telemetry_auto.log_tool_usage(TOOL_NAME)
+##############################################################################################
 
 # -----------------------------------------------------------------------------------
 # SETUP
